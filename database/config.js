479406037048f8e5b1474f8c7e4d81fd19a0c0b2
Jordan Estes <jordan.estes@gmail.com>
2019-04-08 12:48:07 -0500

    created sequelize db

--- database/config.js
+++ database/config.js
@@ -1,2 +1,19 @@
 const mysql = require('mysql');
 const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('bandtogether', 'root', 'password', {
+    host: 'localhost',
+    dialect: mysql,
+})
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+
+
