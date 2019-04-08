3652fb74d739c5c5557216d8eea0ac3b1939a141
Jordan Estes <jordan.estes@gmail.com>
2019-04-08 13:23:37 -0500

    went back to raw SQL finished schema and created connection

--- database/config.js
+++ database/config.js
@@ -1,19 +1,10 @@
 const mysql = require('mysql');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('bandtogether', 'root', 'password', {
-    host: 'localhost',
-    dialect: mysql,
-})
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch((err) => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-
-
-
+const connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: 'password',
+   database: 'bandtogether',
+ });
+ 
