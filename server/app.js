cb5525a5a71411e0dfe735e39d052bc8ebf8379f
Adam Hebert <adam.hebert656@gmail.com>
2019-04-08 21:48:20 -0500

    Fixed server to actually run & serve static files.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const musician = require('./musician');
 const band = require('./band');
 const listing = require('./listing');
 require('dotenv').config();
 
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
 app.use('/musician', musician);
 app.use('/band', band);
 app.use('/listing', listing);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
