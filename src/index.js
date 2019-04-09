1bfdd8b8a8853bcb2b1ca372721d059d72d96958
Adam Hebert <adam.hebert656@gmail.com>
2019-04-08 22:23:12 -0500

    Added container-fluid class to main app component.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <h1>React App</h1>
       </div>
     )
