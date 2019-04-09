b523e2108b7af87796cf5b3bbe30e3399e4cc271
Jordan Estes <jordan.estes@gmail.com>
2019-04-08 22:04:31 -0500

    fixed account and schema to have a boolean for solo or not

--- mockData/account.js
+++ mockData/account.js
@@ -1,6 +1,7 @@
 const data = [
     {
         id: 0,
+        solo: 0,
         username: 'JonnyT',
         password: 'pword',
         salt: 'salt',
@@ -8,6 +9,7 @@ const data = [
     },
     {
         id: 1,
+        solo: 1,
         username: 'BobbyB',
         password: 'BigB123',
         salt: 'salt',
@@ -15,6 +17,7 @@ const data = [
     }, 
     {
         id: 2,
+        solo: 1,
         username: 'KellyH',
         password: 'thisOldStep',
         salt: 'salt',
@@ -22,6 +25,7 @@ const data = [
     }
     {
         id: 3,
+        solo: 0,
         username:'MickeyG',
         password: 'heyyyyy',
         salt: 'salt',
@@ -29,6 +33,7 @@ const data = [
     },
     {
         id: 4,
+        solo: 0,
         username:'JennyK',
         password: '2cool',
         salt: 'salt',
