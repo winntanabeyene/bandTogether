4dbb113ded3301184ee1eb07f650ac619181c7ec
Adam Hebert <adam.hebert656@gmail.com>
2019-04-09 13:03:22 -0500

    Implemented searching.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,23 +3,65 @@ import React from 'react';
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      valueCity: '',
+      valueSearch: '',
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCity = this.handleCity.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleChange(event) {
+    const { id, value } = event.target;
+    if (id === 'city') {
+      this.setState({
+        valueCity: value,
+      });
+    } else if (id === 'search') {
+      this.setState({
+        valueSearch: value,
+      });
+    }
+  }
+
+  handleCity(event) {
+    const { valueCity } = this.state;
+    console.log(valueCity);
+    this.setState({
+      valueCity: '',
+    })
+    // call a function to change currentCity state on home component
+    event.preventDefault();
+  }
+
+  handleSearch(event) {
+    const { valueSearch } = this.state;
+    console.log(valueSearch);
+    this.setState({
+      valueSearch: '',
+    })
+    // call a function to search listings
+    event.preventDefault();
   }
 
   render() {
+    const { valueCity, valueSearch } = this.state;
     return (
-      <div style={{ border: '1px solid red' }} className="jumbotron"> 
+      <div className="jumbotron bg-secondary"> 
         <div className="row">
           <div className="col-md-4">
-            <form>
+            <form onSubmit={this.handleCity}>
               <div className="row" style={{ justifyContent: 'center' }}>
-                <input type="text"></input>
+                <input type="text" placeholder="Enter a City Name" id="city" value={valueCity} onChange={this.handleChange}></input>
                 <button className="btn btn-dark">Change City</button>
               </div>
+            </form>
               <br />
+            <form onSubmit={this.handleSearch}>
               <div className="row" style={{ justifyContent: 'center' }}>
-                <input type="text"></input>
+                <input type="text" placeholder="Search Here" id="search" value={valueSearch} onChange={this.handleChange}></input>
                 <button className="btn btn-dark">Search</button>
               </div>
             </form>
@@ -28,17 +70,17 @@ class Search extends React.Component {
             <div className="row" style={{ justifyContent: 'center' }}>
               <h3>Filter By: </h3>
               <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                <label className="btn btn-secondary active">
-                  <input type="checkbox" />Looking 4 Gig
+                <label className="btn btn-dark">
+                  <input type="checkbox" autoComplete="off" />Looking 4 Gig
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="checkbox" />Looking 4 Band
+                <label className="btn btn-dark">
+                  <input type="checkbox" autoComplete="off" />Looking 4 Band
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="checkbox" />Looking 4 Bandmates
+                <label className="btn btn-dark">
+                  <input type="checkbox" autoComplete="off" />Looking 4 Bandmates
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="checkbox" />Looking 4 Musicians
+                <label className="btn btn-dark">
+                  <input type="checkbox" autoComplete="off" />Looking 4 Musicians
                 </label>
               </div>
             </div>
@@ -46,17 +88,17 @@ class Search extends React.Component {
             <div className="row" style={{ justifyContent: 'center' }}>
               <h3>Sort By: </h3>
               <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                <label className="btn btn-secondary active">
-                  <input type="radio" />Date
+                <label className="btn btn-dark">
+                  <input type="radio" autoComplete="off" />Date
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="radio" />Genre
+                <label className="btn btn-dark">
+                  <input type="radio" autoComplete="off" />Genre
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="radio" />Band Name
+                <label className="btn btn-dark">
+                  <input type="radio" autoComplete="off" />Band Name
                 </label>
-                <label className="btn btn-secondary active">
-                  <input type="radio" />Event Name
+                <label className="btn btn-dark">
+                  <input type="radio" autoComplete="off" />Event Name
                 </label>
               </div>
             </div>
