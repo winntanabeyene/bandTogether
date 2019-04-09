d5c17b21326b3e129b7c57a5d38196c991128ec1
Adam Hebert <adam.hebert656@gmail.com>
2019-04-09 10:00:19 -0500

    Created component files for navbar.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Navbar from './components/Navbar.jsx';
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <h1>React App</h1>
       </div>
     )
