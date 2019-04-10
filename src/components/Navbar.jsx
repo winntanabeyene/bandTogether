7de46653f8dca933de47ee8343b85f232832f4bb
Adam Hebert <adam.hebert656@gmail.com>
2019-04-10 11:59:36 -0500

    Implemented changing view from navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Navbar = (props) => {
+  const { changeView } = props;
   return (
     <div>
       <nav className="navbar fixed-top navbar-dark bg-dark">
-        <a href="#!" className="navbar-brand">Band Together</a>
+        <a href="#!" onClick={() => {changeView('home')}} className="navbar-brand">Band Together</a>
 
         <ul className="nav nav-pills">
           <li className="nav-item">
           {/* When these links are clicked, it should change the view on the state, changing the view that is rendered*/}
-            <a className="nav-link text-light" href="#!">Home</a>
+            <a className="nav-link text-light" onClick={() => {changeView('home')}} href="#!">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-light" href="#!">Profile</a>
+            <a className="nav-link text-light" onClick={() => {changeView('profile')}} href="#!">Profile</a>
           </li>
         </ul>
       </nav>
