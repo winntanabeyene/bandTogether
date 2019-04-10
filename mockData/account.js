d0cf9ceae688682f5b956d8e4845f76ad0682b44
Jordan Estes <jordan.estes@gmail.com>
2019-04-10 13:45:10 -0500

    fixed mock data

--- mockData/account.js
+++ mockData/account.js
@@ -1,41 +1,41 @@
 const data = [
     {
         id: 0,
-        solo: 0,
         username: 'JonnyT',
         password: 'pword',
+        email: 'jonny@gmail.com',
         salt: 'salt',
 
     },
     {
         id: 1,
-        solo: 1,
         username: 'BobbyB',
         password: 'BigB123',
+        email: 'bobby@gmail.com',
         salt: 'salt',
 
     }, 
     {
         id: 2,
-        solo: 1,
         username: 'KellyH',
         password: 'thisOldStep',
+        email: 'kellyh@gmail.com',
         salt: 'salt',
 
     },
     {
         id: 3,
-        solo: 0,
         username:'MickeyG',
         password: 'heyyyyy',
+        email: 'Vomit@gmail.com',
         salt: 'salt',
 
     },
     {
         id: 4,
-        solo: 0,
         username:'JennyK',
         password: '2cool',
+        email: 'chillgirl420@gmail.com',
         salt: 'salt',
 
     }
