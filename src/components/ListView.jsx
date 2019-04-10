cab0a7f5f5572b2d92bf0ec96b450944a4b82116
Adam Hebert <adam.hebert656@gmail.com>
2019-04-10 11:27:41 -0500

    Removed border from Listings.

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ListItem from './ListItem.jsx';
 
 const ListView = ({listings, bands, musicians}) => (
-    <div style={{ border: '2px solid black', paddingTop: "5px" }} className="jumbotron bg-secondary">
+    <div style={{ paddingTop: "5px" }} className="jumbotron bg-secondary">
         <div style={{ justifyContent: 'center' }} className="row">
             <h2 className="text-white">Listings for All Cities</h2>
         </div>
