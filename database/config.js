95a7600aa29a3f84017297c1d41c7e945b6b0774
queyrouzec <queyrouzec@gmail.com>
2019-04-10 20:52:23 -0500

    Refactored database. Made a function to add an account.

--- database/config.js
+++ database/config.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Creates connection. As a matter of flexiblity, security, and habit the password has been moved to .env
-// May move user name to .env incase of same reasons stated for password 
+// Creates connection. As a matter of flexiblity, security, and habit the password and username is in the .env
 const sequelize = new Sequelize('bandtogether', process.env.SQL_USER, process.env.SQL_PASSWORD, {
   host: 'localhost',
   // port: 3306, // default port for mysql. There incase anyone needs it.
@@ -19,17 +18,14 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
-// Any notes beyond this point may be temporary and used to help explain how sequelize works
 
-// Sets up a model to create classes for tables. May need to move all tables to a different file for convention sake
 const Model = Sequelize.Model;
 
-// circular dependances are problemact, therefore tabels with no foriegn keys are listed first
 
-// these tables are made from classes so all the classes holding them will be capitalized for convention and understanding
+
+// The basic account information required to make an account and used to log in. Will have a one-to-one 
+// relationship as the source with artist
 class Account extends Model {};
-
-// creates new table if it hasn't been made already. Methods made for these columns by sequelize are not created until sync and a class instantiation.
 Account.init({
   username: {
     type:Sequelize.STRING,
@@ -46,88 +42,58 @@ Account.init({
     unique: true,
     allowNull: false,
   },
+  email: {
+    type:Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
 }, {
   sequelize, 
-  modelName: 'account', // because the schema doesn't have caps I wanted to remove the capital letter at the start.
-  freezeTableName: true, // keeps table from pluralizing name.
-  underscored: true, // when an association is made with this tables id it will make it user_id as opposed to userId.
-                     // if a primary key is set then associations will use that key name in place of id
-                      // ex: superid being the name of the new primary key, will make a col user_superid or userSuperid.
-});
-
-
-class Instrument extends Model {};
-Instrument.init({
-  name: Sequelize.STRING,
-}, {
-  sequelize,
-  modelName: 'instrument',
+  modelName: 'account',
   freezeTableName: true,
   underscored: true,
 });
 
-// from here tables will be created who's dependencies are already made. Still ignoring circular dependancies for now.
 
-class Musician extends Model {};
-
-// foriegn keys can be created in the init constructor, but will be made in functions following them instead.
-
-Musician.init({
-  name: Sequelize.STRING,
-  city: Sequelize.STRING,
-  age: Sequelize.INTEGER,
-  genre: Sequelize.STRING,
-}, {
-  sequelize,
-  modelName: 'musician',
-  freezeTableName: true,
-  underscored: true,
-});
-
-// MAY BE ABLE TO USE BELONGSTO AND HASONE TOGETHER. NOT 100% SURE.
-
-Musician.belongsTo(Account); // adds a account_id row to hold the account reference. 
-                            // One-to-one association.
-                            // a second argument with an object that has a few useful properties 
-                            // one property is foreignKey that allows you to directly name the column that is created.
-                            // so if we wanted to name the row the same way it is written in the schema we'd do this.
-                              // Musician.belongsTo(Account, {foreignKey: 'id_musician'})
-                            // creates .getAccount, .setAccount, and createAccount methods on Musician instantiation after sync.
-                              // Musician.belongsTo(Account, {as: 'Acc'}) will set the names of these methods to setAcc, getAcc and createAcc.
-                                // very useful for joining a table twice.
-                            // .hasOne method does the same thing as the belongsTo method except Musician and Account reverse rolls.
-                              // in addition .getAccount and .setAccount will become .getMusician, setMusician, and createMusician on the Account instantiation.
-                            // there is also a targetKey and sourceKey (for belongsTo and hasOne respectively) property to designate a reference besides the primary key. We don't use that so I'm not worried about it.
-Account.hasOne(Musician);
-
-
-class Band extends Model {};
-Band.init({
-  name: Sequelize.STRING,
+// Basic info on the Artist. Will have a one-to-one relationship with Account through an account_id column.
+// Will have a one-to-many relationship as the source with listings
+class Artist extends Model {};
+Artist.init({
+  name: {
+    type:Sequelize.STRING,
+    allowNull: false,
+  },
   city: Sequelize.STRING,
   state: Sequelize.STRING,
   genre: Sequelize.STRING,
-  year_formed: Sequelize.INTEGER,
+  solo: Sequelize.BOOLEAN,
+  birtday: Sequelize.INTEGER,
+  url_image: Sequelize.STRING,
+  bio: Sequelize.STRING(500), // may need to increase
+  url_bandcamp: Sequelize.STRING,
+  url_facebook: Sequelize.STRING,
+  url_spotify: Sequelize.STRING,
+  url_homepage: Sequelize.STRING,
+  contact_email: Sequelize.STRING, // need to make one of the last three required
+  contact_num: Sequelize.INTEGER, 
+  contact_facebook: Sequelize.STRING,
 }, {
   sequelize,
-  modelName: 'band',
+  modelName: 'artist',
   freezeTableName: true,
   underscored: true,
-}); 
+});
 
-Band.belongsTo(Account);
-Account.hasOne(Band);
-
-
+// holds the listings for bands to get together. Has a one-to-many relationship as the target with Artist. Col name of
+// foriegn key will be artist_id.
 class Listing extends Model {};
-
 Listing.init({
   title: Sequelize.STRING,
-  date: Sequelize.STRING,
-  description: Sequelize.STRING(1500), // will probably need to extend the length.
+  date: Sequelize.INTEGER,
+  description: Sequelize.STRING(700),
   venue: Sequelize.STRING,
   type: Sequelize.STRING,
-  url_image: Sequelize.STRING,
+  image_url: Sequelize.STRING,
 }, {
   sequelize,
   modelName: 'listing',
@@ -135,63 +101,19 @@ Listing.init({
   underscored: true,
 });
 
-Musician.hasMany(Listing); // same as belongsTo just for a one-to-many relationship
-Band.hasMany(Listing);
+Artist.belongsTo(Account);
+Account.hasOne(Artist);
 
+Artist.hasMany(Listing);
 
-class Profile extends Model {};
-
-Profile.init({
-  url_image: Sequelize.STRING,
-  bio: Sequelize.STRING(500), // may need to increase
-  url_bandcamp: Sequelize.STRING,
-  url_spotify: Sequelize.STRING,
-  url_facebook: Sequelize.STRING,
-  url_homepage: Sequelize.STRING,
-}, {
-  sequelize,
-  modelName: 'profile',
-  freezeTableName: true,
-  underscored: true,
-});
-
-Profile.belongsTo(Musician);
-Profile.belongsTo(Band);
-
-// the many-to-many method .belongsToMany must be used both ways and in the object as its second argument must have the property through to define and/or create the table.
-
-Band.belongsToMany(Musician, {through: 'band_musician'});
-Musician.belongsToMany(Band, {through: 'band_musician'});
-// can do this as Band.belongsToMany(Musician, {through: 'band_musician', otherKey: 'musician_id'}); but I don't know what the consequences are.
-// if you want to own yourself you can do this Band.belongsToMany(Band, {through: 'owned'});
-
-// band_musician table has been created.
-// getBands, setBands, addBand, and addBands are now on Musician instantiations
-// getMusicians, setMusicians, add Musician, and addMusicians are now on Band instantiations
-// when using add property you can set it equal to an object with plural and singluar properties in order to name these methods like so:
-  // Band.belongsToMany(Musician, {through: 'band_musician', as: {singluar: 'band', plural: 'bands'}})
-
-Musician.belongsToMany(Instrument, {through: 'musician_instrument'});
-Instrument.belongsToMany(Musician, {through: 'musician_instrument'});
-
-
-// turns out no tables were circular, so just keep in mind that circular tables can cause problems and you will need to 
-// set the constrains property to false in the relationship method second argument like so:
-  // Musician.hasOne(Account, {constrains: false});
-  // See docs for more info.
-
-
+// must sync to create tabels and associations.
 sequelize.sync()
   .then(() => {
   })
   .catch(err => {console.error(err)});
-  
+
 module.exports = {
   Account,
-  Musician,
-  Band,
+  Artist,
   Listing,
-  Profile,
-  Instrument,
 };
-  
--- database/index.js
+++ database/index.js
@@ -1,28 +1,61 @@
-const { Account, Musician, Band, Listing, Profile, Instrument } = require('./config.js/index.js');
-
-const account = new Account();
-const musician = new Musician();
-const band = new Band();
-const listing = new Listing();
-const profile = new Profile();
-const instrument = new Instrument();
-// don't know how to use the get<column> method yet, but these are here for that.
-
-// sudo coded relationships like this so that we can get used to reading and using sequelize
-// Musician.belongsTo(Account);
-// Account.hasOne(Musician); // may cause errors, may just add functionality to the instatiation of Account
-// Band.belongsTo(Account);
-// Musician.hasMany(Listing);
-// Band.hasMany(Listing);
-// Listing.belongsTo(Musician);
-// Listing.belongsTo(Band);
-// Band.belongsToMany(Musician, {through: 'band_musician'});
-// Musician.belongsToMany(Band, {through: 'band_musician'});
-// Musician.belongsToMany(Instrument, {through: 'musician_instrument'});
-// Instrument.belongsToMany(Musician, {through: 'musician_instrument'});
+const { Account, Artist, Listing } = require('./config.js');
 
 
 
+/**
+ * makes an object with certain variables, ignoring ones that are undefined.
+ * @param {object} object - the object who's props are being taken.
+ * @param {Array} propsToTake - a list of properties to add to the object.
+ * @returns {Object}
+ */
+const makeObject = (object, propsToTake) => {
+  let returnObject = {}
+  for(let key in object) {
+    if (propsToTake.indexOf(key) !== -1 && !!object[key]) {
+      returnObject[key] = object[key];
+    }
+  }
+  return returnObject;
+}
+
+/**
+ * creates an account and an associated band or musician. I haven't worked out the kinks for returning an error yet, so for now,
+ * it just console.errors an error message.
+ * @param {object} accDetails - requires the properties of username, password, salt, name, solo, and email are all required.
+ * city, state, genre, birthdate, image_url, url_image, bio, url_bandcamp, url_facebook, url_spotify, url_homepage, 
+ * contact_email, contact_num, or contact_facebook.
+ * @returns {Promise} 
+ */
+const makeAccount = (accDetails) => {
+  const {username, password, salt, email, name, solo} = accDetails;
+  if(!username || !password || !salt || !name || !solo || !email) {
+    console.error("Attempted to make acc without required fields.")
+    return;
+  }
+  const artistObj = makeObject(accDetails, ['name', 'solo', 'city', 'state', 'genre', 'birthdate', 'image_url', 'url_image', 'bio', 
+    'url_bandcamp', 'url_facebook', 'url_spotify', 'url_homepage', 'contact_email', 'contact_num', 'contact_facebook'])
+  // makes account
+  return Account.create({ username, password, salt, email })
+  // makes artist
+  .then(account => Artist.create(artistObj)
+    // associates the new artist and account
+    .then(artist => artist.setAccount(account.id))
+  )
+};
 
 
-module.exports = {};
+
+/**
+ * updates an account by id or username. If both are given will use id. Only adds or changes information. Does not remove it.
+ * @param {object} account - must have an account by id or username. 
+ * @param {object} update - properties to change. All are optional
+ */
+const updateAccount = (account, update) => {
+  
+};
+
+
+
+module.exports = {
+  makeAccount,
+};
