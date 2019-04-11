e7732ad7a1bec2f7cb5029ee28bfa959fa331f79
queyrouzec <queyrouzec@gmail.com>
2019-04-11 02:26:58 -0500

    Finished Middleware for database.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const { Account, Artist, Listing } = require('./config.js');
 
-const optionalValues = ['city', 'state', 'genre', 'birthdate', 'url_image', 'bio', 'url_bandcamp', 'url_facebook', 
+
+// start of profile and account Middleware
+const optionalProfileValues = ['city', 'state', 'genre', 'birthdate', 'url_image', 'bio', 'url_bandcamp', 'url_facebook', 
   'url_spotify', 'url_homepage', 'contact_email', 'contact_num', 'contact_facebook'];
 
 /**
@@ -55,10 +57,10 @@ const makeSearchObject = (account) => {
 const makeAccount = (accDetails) => {
   const {username, password, salt, email, name, solo} = accDetails;
   if(!username || !password || !salt || !name || !solo || !email) {
-    console.error("Attempted to make acc without required fields.")
+    console.error("Attempted to make an account without required fields.")
     return;
   }
-  const artistObj = makeObject(accDetails, ['name', 'solo'].concat(optionalValues))
+  const artistObj = makeObject(accDetails, ['name', 'solo'].concat(optionalProfileValues))
   // makes account
   return Account.create({ username, password, salt, email })
   // makes artist
@@ -80,7 +82,7 @@ const makeAccount = (accDetails) => {
 const updateArtistDetails = (account, update) => {
   const acc = makeSearchObject(account);
   if (!acc) return; 
-  const updateObject = makeObject(update, optionalValues);
+  const updateObject = makeObject(update, optionalProfileValues);
   return Account.findOne(acc)
   // gets the associated artist
   .then(account => account.getArtist())
@@ -97,11 +99,14 @@ const updateArtistDetails = (account, update) => {
  * url_bandcamp, url_facebook, url_spotify, url_homepage, contact_email, contact_num, contact_facebook.
  */
 const deleteArtistData = (account, removeValues) => {
+  if(!Array.isArray(removeValues)){
+    removeValues = [removeValues];
+  }
   const acc = makeSearchObject(account);
   if (!acc) return;
   // makes an object to delete
   const updateObject = removeValues.reduce((seed, value) => {
-    if(value.indexOf(optionalValues) !== -1) {
+    if(optionalProfileValues.indexOf(value) !== -1) {
       seed[value] = null;
     }
     return seed;
@@ -140,11 +145,106 @@ const getProfileInformation = (account) => {
     .then(artist => {
       // builds object to be returned in promis
       const profileObject = {id: account.id, email: account.email, name: artist.name};
-      optionalValues.forEach(value => {
+      optionalProfileValues.forEach(value => {
         profileObject[value] = artist[value];
       });
       return profileObject;
     }));
+};
+
+
+
+// start of listing Middleware
+
+const listingValues = ['title', 'date', 'description', 'venue', 'type', 'image_url'];
+
+/**
+ * Makes a new listing.
+ * @param {Object} account - must have an account by id or username listed in the account table. If both are given will use id.
+ * @param {object} newListing - must have title, date, description, and venue.
+ * @returns {object} - sequelize promise with the artist of the listing.
+ */
+const makeListing = (account, newListing) => {
+  const acc = makeSearchObject(account);
+  if (!acc) return;
+  const {title, date, description, venue} = newListing;
+  if (!title || !date || !description || !venue) {
+    console.error("Attempted to make a listing without required fields.")
+    return;
+  }
+  const newListingObject = makeObject(newListing, listingValues);
+  return Account.findOne(acc)
+    .then(account => account.getArtist())
+    .then(artist => Listing.create(newListingObject)
+      .then(listing => artist.addListing(listing.id))
+    );
+};
+
+
+
+/**
+ * 
+ * @param {object} filter - fields include title, date, description, venue, type, and image_url. leaving this object empty
+ * or undefined returns all listings.
+ */
+const getListings = (filter = {}) => {
+  const listingFilters = makeObject(filter, listingValues.concat(['id', 'artist_id']));
+  return Listing.findAll({where: listingFilters});
+};
+
+
+
+/**
+ * 
+ * @param {number} id - id of the listing being updated.
+ * @param {object} update - an object with listing properties to update. 
+ * @returns {Promise}
+ */
+const updateListings = (id, update) => {
+  const updateObject = makeObject(update, listingValues);
+  return Listing.update(updateObject, {where: {id}})
+};
+
+
+
+/**
+ * removes type and/or image_url from a table row.
+ * @param {number} id - id of the listing being effected.
+ * @param {array} propsToRemove - contains a string of type and/or image.
+ */
+const deleteListingProperties = (id, propsToRemove) => {
+  const updateObject = {};
+  if(propsToRemove.indexOf("type") !== -1) {
+    updateObject.type = null;
+  }
+  if(propsToRemove.indexOf("image_url") !== -1) {
+    updateObject.image_url = null;
+  }
+  return Listing.update(updateObject, {where: {id}});
+};
+
+
+
+/**
+ * @param {number} id - id of listing to be deleted. 
+ * @return {Promise} - sequelize promise containing the deleted data.
+ */
+const deleteListing = (id) => {
+  return Listing.findOne({id})
+    .then(listing => listing.destroy());
+};
+
+
+
+/**
+ * 
+ * @param {NUMBER} id - number
+ * @returns array of sequalize objects.
+ */
+const getListingsByAccountId = (id) => {
+  return Account.findOne({id})
+    .then(account => account.getArtist())
+    .then(artist => getListings());
 };
 
 
@@ -155,4 +255,10 @@ module.exports = {
   deleteArtistData,
   getAccountInformation,
   getProfileInformation,
+  makeListing,
+  getListings,
+  updateListings,
+  deleteListingProperties,
+  deleteListing,
+  getListingsByAccountId,
 };
