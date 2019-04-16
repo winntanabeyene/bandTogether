71f749bf0b9ec89b10cc5ee7709d50c0ae593a66
Winntana Beyene <winntanab@gmail.com>
2019-04-16 16:16:21 -0500

    changed mysql password

--- database/config.js
+++ database/config.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const USER = process.env.SQL_USER || 'root';
-const PASSWORD = process.env.SQL_PASSWORD || 'password';
+const PASSWORD = process.env.SQL_PASSWORD || '';
 
 // Creates connection. As a matter of flexiblity, security, and habit the password has been moved to .env
 // May move user name to .env incase of same reasons stated for password 
 const sequelize = new Sequelize('bandtogether', USER, PASSWORD, {
   host: 'localhost',
-  // port: 3306, // default port for mysql. There incase anyone needs it.
+   port: 3306, // default port for mysql. There incase anyone needs it.
   dialect: 'mysql',
 });
 
