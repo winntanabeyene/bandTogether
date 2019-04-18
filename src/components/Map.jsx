c4a4cab36e6f1f84182459c93b4724a257471ae6
amanriquez <amanriquez.guitar@gmail.com>
2019-04-18 16:44:30 -0500

    started map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Axios from 'axios';
+
+
+class Map extends React.Component{
+
+constructor(props){
+  super(props)
+
+  this.state = {
+    listings: [],
+    centerOfUs: [39.8283, -98.5795]
+  }
+
+this.getCoordinates = this.getCoordinates.bind(this);
+this.getMap = this.getMap.bind(this);
+}
+
+
+
+getMap(){
+
+}
+
+
+getCoordinates(){
+
+Axios.
+
+
+}
+
+
+render() {
+
+
+
+
+
+
+  return(
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+
+
+
+}
+
+export default Map;
